229b6f83322e1eb4aaa6215fef03513cf21ffcd8
angtdiaz <nicoltuarezd@gmail.com>
2020-06-25 19:48:56 -0500

    Clase Postre

---
+++ TallerSOLID/src/Postres/Postre.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Postres;
+
+import Adicionales.Aderezo;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Postre {
+    protected String sabor;
+    protected double precioParcial;
+    protected ArrayList<Aderezo> aderezos;
+    
+    public double calcularPrecioFinal(){
+        double precioFinal;
+        precioFinal=(precioParcial+(precioParcial*0.12))+(aderezos.size()*0.50);
+        return precioFinal;
+    }
+    public String showPrecioFinal(){
+        return "Precio Final: $ " + calcularPrecioFinal();
+    }
+
+    public Postre(String sabor) {
+        aderezos= new ArrayList<>();
+        this.sabor = sabor;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public double getPrecioParcial() {
+        return precioParcial;
+    }
+
+    public void setPrecioParcial(double precioParcial) {
+        this.precioParcial = precioParcial;
+    }
+
+    public ArrayList<Aderezo> getAderezos() {
+        return aderezos;
+    }
+
+    public void addAderezo(Aderezo ad){
+        aderezos.add(ad);
+    }
+    
+    public void removeAderezo(Aderezo ad){
+        aderezos.remove(ad);
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "Postre{" + "sabor=" + sabor + ", precioParcial=" + precioParcial + ", aderezos=" + aderezos + '}';
+    }
+
+    
+    
+}
